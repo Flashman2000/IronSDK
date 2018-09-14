3cf27fb515b074160edd852e51f61e527d23507c
Flash <32598745+Flashman2000@users.noreply.github.com>
2018-09-13 23:10:43 -0500

    Beta Version of the RC Opmode

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/IronDrive_BETA.java
@@ -0,0 +1,9 @@
+package org.firstinspires.ftc.teamcode;
+
+
+import com.qualcomm.robotcore.eventloop.opmode.Disabled;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+
+@TeleOp(name="IronDrive_BETA", group="Linear Opmode")
+public class IronDrive_BETA {
+}
