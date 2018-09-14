078ed3753c7a47ed80767c2ad292e9b62b8d6afa
Flash <32598745+Flashman2000@users.noreply.github.com>
2018-09-13 23:33:34 -0500

    1.0_BETA

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/IronDrive_BETA.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/IronDrive_BETA.java
@@ -2,8 +2,36 @@ package org.firstinspires.ftc.teamcode;
 
 
 import com.qualcomm.robotcore.eventloop.opmode.Disabled;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.util.ElapsedTime;
 
 @TeleOp(name="IronDrive_BETA", group="Linear Opmode")
-public class IronDrive_BETA {
+public class IronDrive_BETA extends OpMode {
+
+    robot ironclad = new robot();
+
+    @Override
+    public void init(){ ironclad.init(hardwareMap); }
+
+    @Override
+    public void init_loop() {}
+
+    @Override
+    public void start(){}
+
+    @Override
+    public void loop(){
+        double left;
+        double right;
+
+        left = -gamepad1.left_stick_y;
+        right = -gamepad1.right_stick_y;
+
+        ironclad.leftDrive.setPower(left);
+        ironclad.rightDrive.setPower(right);
+    }
+
 }
---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/robot.java
@@ -0,0 +1,42 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DcMotorSimple;
+import com.qualcomm.robotcore.hardware.HardwareDevice;
+import com.qualcomm.robotcore.hardware.HardwareMap;
+import com.qualcomm.robotcore.util.ElapsedTime;
+
+public class robot {
+
+    public DcMotor leftDrive = null;
+    public DcMotor rightDrive = null;
+
+    HardwareMap hwm = null;
+    private ElapsedTime period = new ElapsedTime();
+
+    public robot(){}
+
+    public void init(HardwareMap ahwm){
+
+        hwm = ahwm;
+
+        leftDrive = hwm.get(DcMotor.class, "leftDrive");
+        rightDrive = hwm.get(DcMotor.class, "rightDrive");
+
+        leftDrive.setDirection(DcMotorSimple.Direction.REVERSE);
+        rightDrive.setDirection(DcMotorSimple.Direction.FORWARD);
+
+        leftDrive.setPower(0);
+        rightDrive.setPower(0);
+
+        leftDrive.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+        rightDrive.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+
+        leftDrive.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        rightDrive.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+    }
+
+
+
+
+}
