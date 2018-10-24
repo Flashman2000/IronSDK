f34f15ffa8d21412665ff87a8a2b90f8ca77ab23
Flash <32598745+Flashman2000@users.noreply.github.com>
2018-10-23 19:16:53 -0500

    0.4.1-beta

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Ironclad/robot.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Ironclad/robot.java
@@ -1,8 +1,5 @@
 package org.firstinspires.ftc.teamcode.Ironclad;
 
-import com.disnodeteam.dogecv.CameraViewDisplay;
-import com.disnodeteam.dogecv.DogeCV;
-import com.disnodeteam.dogecv.detectors.roverrukus.SamplingOrderDetector;
 import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.DcMotorSimple;
 import com.qualcomm.robotcore.hardware.Gamepad;
@@ -15,12 +12,9 @@ public class robot {
 
     public DcMotor leftDrive = null;
     public DcMotor rightDrive = null;
-    public String Right = "Right";
-    public String Left = "Left";
-    public String Center = "Center";
-    public String Unknown = "Unknown";
-
-    public SamplingOrderDetector detector;
+    public DcMotor horzSlide = null;
+    public DcMotor vertSlide = null;
+    public DcMotor linActuator = null;
 
     HardwareMap hwm = null;
     Telemetry tele = null;
@@ -35,36 +29,35 @@ public class robot {
 
         leftDrive = hwm.get(DcMotor.class, "leftDrive");
         rightDrive = hwm.get(DcMotor.class, "rightDrive");
+        horzSlide = hwm.get(DcMotor.class, "horzSlide");
+        vertSlide = hwm.get(DcMotor.class, "verSlide");
+        linActuator = hwm.get)DcMotor.class, "linAct");
 
         leftDrive.setDirection(DcMotorSimple.Direction.REVERSE);
         rightDrive.setDirection(DcMotorSimple.Direction.FORWARD);
+        horzSlide.setDirection(DcMotorSimple.Direction.FORWARD);
+        vertSlide.setDirection(DcMotorSimple.Direction.FORWARD);
+        linActuator.setDirection(DcMotorSimple.Direction.FORWARD);
 
         leftDrive.setPower(0);
         rightDrive.setPower(0);
+        horzSlide.setPower(0);
+        vertSlide.setPower(0);
+        linActuator.setPower(0);
 
         leftDrive.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
         rightDrive.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+        horzSlide.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+        vertSlide.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+        linActuator.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
 
         leftDrive.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
         rightDrive.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        horzSlide.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        vertSlide.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        linActuator.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
 
-        tele.addData("Status", "DogeCV 2018.0 - Sampling Order Example");
 
-        detector = new SamplingOrderDetector();
-        detector.init(ahwm.appContext, CameraViewDisplay.getInstance());
-        detector.useDefaults();
-
-        detector.downscale = 0.4; // How much to downscale the input frames
-
-        // Optional Tuning
-        detector.areaScoringMethod = DogeCV.AreaScoringMethod.MAX_AREA; // Can also be PERFECT_AREA
-        //detector.perfectAreaScorer.perfectArea = 10000; // if using PERFECT_AREA scoring
-        detector.maxAreaScorer.weight = 0.001;
-
-        detector.ratioScorer.weight = 15;
-        detector.ratioScorer.perfectRatio = 1.0;
-
-        detector.enable();
     }
 
     public void startRcActivity(Gamepad gp1, Gamepad gp2, Telemetry tel){
@@ -73,26 +66,35 @@ public class robot {
 
         double left;
         double right;
+        double horz = 0;
+        double vert = 0;
+        double lin;
 
         left = -gp1.left_stick_y;
         right = -gp1.right_stick_y;
+        lin = gp1.right_trigger - gp1.left_trigger;
+
+        if(gp2.dpad_right){
+            horz = 1;
+        }else if(gp2.dpad_left){
+            horz = -1;
+        }else{
+            horz = 0;
+        }
+
+        if(gp2.dpad_up){
+            vert = 1;
+        }else if(gp2.dpad_down){
+            vert = -1
+        }else{
+            vert = 0;
+        }
 
         leftDrive.setPower(left);
         rightDrive.setPower(right);
-
-        tele.addData("Current Order" , detector.getCurrentOrder().toString()); // The current result for the frame
-        tele.addData("Last Order" , detector.getLastOrder().toString()); // The last known result
-
-        if (detector.getCurrentOrder() == SamplingOrderDetector.GoldLocation.RIGHT){
-            tele.addData("Position Tester", Right);
-        }else if (detector.getCurrentOrder() == SamplingOrderDetector.GoldLocation.LEFT){
-            tele.addData("Position Tester", Left);
-        }else if (detector.getCurrentOrder() == SamplingOrderDetector.GoldLocation.CENTER){
-            tele.addData("Position Tester", Center);
-        }else{
-            tele.addData("Position Tester", Unknown);
-        }
-        tele.update();
+        horzSlide.setPower(horz);
+        vertSlide.setPower(vert);
+        linActuator.setPower(lin);
 
     }
 
